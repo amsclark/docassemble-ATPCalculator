ce5e6031da4af5bf0d8020e57b421c3f67b99b67
Alex Clark <amsclark@gmail.com>
2024-12-25 10:52:48 -0500

    added other states and counties aside from iowa

--- docassemble/ATPCalculator/irs_housing.py
+++ docassemble/ATPCalculator/irs_housing.py
@@ -7,21 +7,12 @@ county_info_by_name = {}
 county_names = []
 
 def get_total_housing_allowance(county, household_size):
-  if county not in county_info_by_name:
-    raise Exception("Reference to invalid county " + county)
-  if household_size <= 4:
-    return county_info(county)[str(household_size)]
-  else:
-    return county_info(county)["five_or_more"]
-
-def read_data(filename):
-    the_xlsx_file, mimetype = path_and_mimetype(filename)
-    df = pandas.read_excel(the_xlsx_file)
-    for indexno in df.index:
-        if not df['County'][indexno]:
-            continue
-        county_names.append(df['County'][indexno])
-        county_info_by_name[df['County'][indexno]] = {"1": df['One'][indexno], "2": df['Two'][indexno], "3": df['Three'][indexno], "4": df['Four'][indexno], "five_or_more": df['Five_Or_More'][indexno]}
+    if county not in county_info_by_name:
+        raise Exception("Reference to invalid county " + county)
+    if household_size <= 4:
+        return clean_and_convert(county_info(county)[str(household_size)])
+    else:
+        return clean_and_convert(county_info(county)["five_or_more"])
 
 def get_county_names():
     return county_names
@@ -37,11 +28,23 @@ def read_state_data(state_name):
     df = pandas.read_csv(the_tsv_file, sep='\t')
     state_county_info = {}
     state_county_names = []
+    
+    # Map the actual column names to shorter keys
     for indexno in df.index:
         if not df['County'][indexno]:
             continue
         state_county_names.append(df['County'][indexno])
-        state_county_info[df['County'][indexno]] = {"1": df['One'][indexno], "2": df['Two'][indexno], "3": df['Three'][indexno], "4": df['Four'][indexno], "five_or_more": df['Five_Or_More'][indexno]}
+        state_county_info[df['County'][indexno]] = {
+            "1": df['2024 Published ALE Housing Expense for a Family of 1'][indexno],
+            "2": df['2024 Published ALE Housing Expense for a Family of 2'][indexno],
+            "3": df['2024 Published ALE Housing Expense for a Family of 3'][indexno],
+            "4": df['2024 Published ALE Housing Expense for a Family of 4'][indexno],
+            "five_or_more": df['2024 Published ALE Housing Expense for a Family of 5'][indexno]
+        }
+    
+    # Populate the global county_info_by_name with data from the current state's TSV
+    global county_info_by_name
+    county_info_by_name = state_county_info
     return state_county_info, state_county_names
 
 def get_state_county_names(state_name):
@@ -59,8 +62,12 @@ def get_state_total_housing_allowance(state_name, county, household_size):
     if county not in state_county_info:
         raise Exception(f"Reference to invalid county {county} in state {state_name}")
     if household_size <= 4:
-        return state_county_info[county][str(household_size)]
+        return clean_and_convert(state_county_info[county][str(household_size)])
     else:
-        return state_county_info[county]["five_or_more"]
+        return clean_and_convert(state_county_info[county]["five_or_more"])
 
-read_data('data/sources/irs_housing_data.xlsx')
+def clean_and_convert(value):
+    # Remove non-numeric characters (like $ and commas) and convert to float
+    if isinstance(value, str):
+        return float(value.replace('$', '').replace(',', ''))
+    return float(value)

6ebf1e0f304cbe9c804c3805c7567592948972ae
Alex Clark <amsclark@gmail.com>
2024-12-26 08:05:44 -0500

    fixed puerto rico. fixed transport standard retrieval

--- docassemble/ATPCalculator/transportation_standards.py
+++ docassemble/ATPCalculator/transportation_standards.py
@@ -2,17 +2,16 @@ import pandas as pd
 from docassemble.base.util import path_and_mimetype
 
 def get_transportation_standards():
-    filename = f"data/sources/Transportation_Standards.csv"
-    try:
-        # Read the CSV file
-        df = pd.read_csv(filename)
-        return df
-    except FileNotFoundError:
-        print(f"Error: The file '{filename}' was not found in the current directory.")
-        return "FNF"
-    except pd.errors.EmptyDataError:
-        print(f"Error: The file '{filename}' is empty.")
-        return "Empty"
-    except Exception as e:
-        print(f"An error occurred while reading the file: {e}")
-        return e
+  try:
+    transport_filename = f"data/sources/transportationstandards.csv"
+    the_csv_file, csv_file_mimetype = path_and_mimetype(transport_filename)
+    with open(the_csv_file, "r") as f:
+      transportation_standards = pd.read_csv(f)
+    return transportation_standards
+  except FileNotFoundError:
+    return "File Not Found"
+  except pd.errors.EmptyDataError:
+    return "The file is empty"
+  except Exception as e:
+    return e
+    
